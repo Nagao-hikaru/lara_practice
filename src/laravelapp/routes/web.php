abfb0b790049c630e8c9c03ba4453ea81f32890d
Nagao-hikaru <koyanagi29sushi@gmail.com>
2021-05-23 17:37:40 +0900

    コントローラー、ルーティングの設定

--- src/laravelapp/routes/web.php
+++ src/laravelapp/routes/web.php
@@ -20,3 +20,4 @@ Route::get('tests/test', 'TestController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('contacts', 'contactFormController');
