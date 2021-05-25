648d7c06f3a986f4dbcaae5c27999dbd513be9b8
Nagao-hikaru <koyanagi29sushi@gmail.com>
2021-05-26 00:10:28 +0900

    routeのauth設定

--- src/laravelapp/routes/web.php
+++ src/laravelapp/routes/web.php
@@ -16,8 +16,11 @@ Route::get('/', function () {
 });
 
 Route::get('tests/test', 'TestController@index');
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function () {
+    // Route::get('index', 'ContactFormController@index');
+    Route::resource('contact', 'ContactFormController');
+});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('contacts', 'contactFormController');
